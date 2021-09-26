66cb83e6ffe8bf14458517da5ba205c1c69375d4
Brett Dorrans <hello@lapidist.net>
2021-09-26 16:18:39 +0100

    fix: webpack-dev-server api changes

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -54,6 +54,29 @@ module.exports = {
                 enforce: 'pre',
                 exclude: /node_modules/,
                 loader: 'source-map-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                            modules: true
+                        }
+                    }
+                ],
+                include: /\.module\.css$/
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+                exclude: /\.module\.css$/
+            },
+            {
+                test: /\.svg$/,
+                use: 'file-loader'
             }
         ]
     },
